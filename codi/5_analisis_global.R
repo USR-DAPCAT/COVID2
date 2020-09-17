82494c981dd939b3930e0bd7e080330c4ece9eb4
unknown <raypuig@gmail.com>
2020-09-17 12:55:51 +0200

    actualitazcio

--- codi/5_analisis_global.R
+++ codi/5_analisis_global.R
@@ -18,7 +18,7 @@ rmarkdown::render(input="./codi/1_lectura_COVID.Rmd",
 #rm(list=ls())
 #dir_dades="dades/SIDIAP"
 rmarkdown::render(input="./codi/3_2_analisi_COVID.Rmd",
-                  output_file="output/informe_exploratori3.html",
+                  output_file="output/informe_exploratori.html",
                   params = list(dir_dades="dades"))
 
 
