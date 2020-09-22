01a1507fa3cd1ccb0584e5409c107477e6501fcb
unknown <raypuig@gmail.com>
2020-09-22 11:44:04 +0200

    actualitzar 3_analisis_global

--- codi/3_analisis_global.R
+++ codi/3_analisis_global.R
@@ -1,5 +1,5 @@
 ################
-# 18//09//2020 #
+# 22//09//2020 #
 ################
 
 rm(list=ls())
@@ -9,6 +9,40 @@ rm(list=ls())
 #ii)  Falta añadir la variable fallo renal agudo en la tabla de eventos[OK]
 #iii) Falta añadir la nueva variable enfermedad cardiovascular en el modelo de mortalidad
 #     DG2.COMP_CARDIOVASC: [ajuste2,ajuste3,ajuste5,ajuste6][ok]
+#iv)  Lo de la glucemia >200 suposo que es perque per la ADA una glucemia de >200 en qualsevol moment del dia es criteri de DM
+#     Pero en algun dels models (per exemple el que fa servir DM categorica)
+#     doncs sí que s'hauria de posar la glucemia com a variable continua (Josep)
+
+#Bon vespre:
+
+# Respecto los foresplot,los puedes hacer en blanco y negro y poner una linia verical?
+
+
+## Els 3 models són:
+## M1: DM + variables clíniques bàsiques (edat i sexe)
+## M2: M1 + obesitat + lesió orgànica (COPD, heart failure, kidney failure, cardiovascular diseases)
+## M3 :M2 + hypertension + dyslipidemia.
+
+## S'em fa estrany veure DM en el primer model, 
+## obesitat en el segon i HTA i dislipèmia en el tercer. 
+## No sé si Dídac i Josep us fa el mateix efecte
+
+## Una altra possibilitat d'agrupació seria:
+## M1: igual
+## M2: M1 + obesitat + HTA + dislipèmia
+## M3 : M2 + lesions orgàniques
+
+#  Amb els models actuals, encara que les OR de les lesions d'orga diana són altes, 
+#  la R2 canvia més del model 2 al 3 que del 1 al 2.  
+#  Afegint HTA i dislipemia a model 2 i complicacions d'organ diana a model 3, 
+#  segurmanet el canvi de R2 seria més progressiu (i més petit a l'últim pas)
+#  Seria similar pels models que utilitzen glucosa continua.
+
+#  El primer forest plot correspon a les taules 3.1 i 3.2 
+#  amb la diferència de que en el forest plot l'edat és >65?? 
+#  El segon a la Taula 5? Ensenyaríem taules i forest plots?
+
+
 
 
 #i)
