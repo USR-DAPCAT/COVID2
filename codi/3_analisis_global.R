08e7358ac4263fd48c10e90dbc8f5e81e162382a
unknown <raypuig@gmail.com>
2020-11-16 18:34:46 +0100

    ACTUALITAZCIó

--- codi/3_analisis_global.R
+++ codi/3_analisis_global.R
@@ -1,11 +1,26 @@
 ################
-# 05//11//2020 #
+# 16//11//2020
+# DILLUNS
 ################
 
 
 rm(list=ls())
 # dir_dades="dades/mostra"
 
+library("dplyr")
+library("lubridate")
+#library("expss")
+library("frequency")
+library("sjPlot")
+library("sjmisc")
+library("sjlabelled")
+library("base")
+library("table1")
+# load package
+library("sjPlot")
+library("sjmisc")
+library("sjlabelled")
+library("mgcViz")
 
 
 #rm(list=ls())
@@ -17,9 +32,12 @@ rmarkdown::render(input="./codi/1_lectura_COVID.Rmd",
 
 #rm(list=ls())
 #dir_dades="dades/SIDIAP"
-rmarkdown::render(input="./codi/3_3_analisi_COVID.Rmd",
-                  output_file="output/informe_exploratori20.html",
+rmarkdown::render(input="./codi/2_analisi_COVID.Rmd",
+                  output_file="output/informe_exploratori.html",
                   params = list(dir_dades="dades"))
 
+#####################################################################
+#Error:[`starts_with()` must be used within a *selecting* function.]#
+#####################################################################
 
 
